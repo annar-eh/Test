7225e54a56bb33ddd8d1f2298d0045476d8fedba
Freia <annar.hilde@gmail.com>
2013-09-17 17:28:13 +0200

    Trololo

---
+++ src/stdafx.h
@@ -0,0 +1,23 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+// TODO: reference additional headers your program requires here
+
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <iostream>
+#include <tchar.h>
+#include <time.h>
+#include <vector>
+#include <process.h>
+#include <string>
+#include <fstream>
+#include <algorithm>
+#include <iomanip>
+#include <stdexcept>
+#include <WinUser.h>
+#include <WinDef.h>
